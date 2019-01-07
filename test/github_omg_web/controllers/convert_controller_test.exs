5b0ee97c7a15a02d26acef7e3ccf2ffcccba8ba3
Dan Klasson <dan.g.klasson@gmail.com>
2019-01-08 06:34:17 +0800

    added empty controller tests

---
+++ test/github_omg_web/controllers/convert_controller_test.exs
@@ -0,0 +1,15 @@
+defmodule GithubOmgWeb.ConvertControllerTest do
+  use GithubOmgWeb.ConnCase
+
+  describe "/api/convert" do
+    test "submitted json returns modified json", %{conn: conn} do
+
+    end
+    test "handles empty json" do
+
+    end
+    test "returns status code 500 on invalid json" do
+
+    end
+  end
+end
